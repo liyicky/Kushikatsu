ea0a332bfdefe843599c160ae73066743fe525c2
liyicky <liyicky@gmail.com>
2019-02-04 14:27:10 +0900

    more controller stuff

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,34 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
   
   def index
     @portfolios = Portfolio.all
   end
+
+  def new
+    @portfolio = Portfolio.new
+  end
+
+  def create
+    @portfolio = Portfolio.create!(portfolio_params)
+    respond_to do |format|
+      if @portfolio.save
+        format.html { redirect_to @portfolio, notice: 'Portfolio was successfully updated.' }
+      end
+    end
+  end
+
+  def show
+  end
+
+end
+
+private
+
+def set_portfolio
+  @portfolio = Portfolio.find(params[:id])
+end
+
+def portfolio_params
+  params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image)
 end
