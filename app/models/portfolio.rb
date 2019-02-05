ec942fed6b1527471c340ab5191976e964545ffd
liyicky <liyicky@gmail.com>
2019-02-05 15:53:05 +0900

    default images

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,7 +1,14 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
 
+  after_initialize :set_defaults
+
   def self.angular
     where(subtitle: 'Angular')
+  end
+
+  def set_defaults
+    self.main_image ||= 'https://via.placeholder.com/600x400'
+    self.thumb_image ||= 'https://via.placeholder.com/350x20'
   end
 end
