6ede28a678aad5f1c1447cf5a9c100e299bf5a16
liyicky <liyicky@gmail.com>
2019-02-05 15:34:31 +0900

    portfolio scopes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+3.times do |topic|
+  Topic.create!(title: "Topic: #{topic}")
+end
+
+puts "3 topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
@@ -16,7 +22,8 @@
     
     <h2>Hold on to your butts</h2>
     Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit. 
-    """
+    """,
+    topic_id: Topic.last.id
   )
 end
 
@@ -31,10 +38,24 @@ end
 
 puts "5 skils created"
 
-9.times do |portfolio|
+8.times do |portfolio|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio}",
     subtitle: "My Great Service",
+    body: 
+    """
+    Fingerstache yuccie slow-carb williamsburg DIY. Palo santo 90's jianbing, coloring book next level YOLO banjo literally put a bird on it tumeric. Taxidermy thundercats humblebrag, marfa flexitarian kombucha food truck green juice tumeric tumblr roof party. Locavore activated charcoal small batch brunch gluten-free health goth brooklyn, enamel pin authentic. Distillery butcher four loko normcore intelligentsia kickstarter shoreditch disrupt microdosing skateboard seitan offal. Fam church-key flexitarian chambray vice normcore lomo mustache live-edge.
+    Farm-to-table whatever disrupt flannel locavore. Hella hammock tousled, bespoke normcore mlkshk vexillologist health goth semiotics actually. Sustainable narwhal irony squid four loko. Pop-up godard kitsch, salvia shaman selvage man bun post-ironic semiotics viral whatever next level 3 wolf moon edison bulb. Deep v fixie waistcoat, freegan try-hard mumblecore ugh live-edge letterpress iPhone beard art party meh scenester copper mug. Shoreditch tofu wayfarers forage affogato asymmetrical.
+    """,
+    main_image: "https://via.placeholder.com/600x400",
+    thumb_image: "https://via.placeholder.com/350x200"
+  )
+end
+
+8.times do |portfolio|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio}",
+    subtitle: "Angular",
     body: 
     """
     Fingerstache yuccie slow-carb williamsburg DIY. Palo santo 90's jianbing, coloring book next level YOLO banjo literally put a bird on it tumeric. Taxidermy thundercats humblebrag, marfa flexitarian kombucha food truck green juice tumeric tumblr roof party. Locavore activated charcoal small batch brunch gluten-free health goth brooklyn, enamel pin authentic. Distillery butcher four loko normcore intelligentsia kickstarter shoreditch disrupt microdosing skateboard seitan offal. Fam church-key flexitarian chambray vice normcore lomo mustache live-edge.
