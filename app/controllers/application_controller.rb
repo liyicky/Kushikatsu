0e43c0329212a3d6a2281df1cda5db07f797ee41
liyicky <liyicky@gmail.com>
2019-02-06 14:09:56 +0900

    refactored devise params into concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
-
-  before_action :config_params, if: :devise_controller?
-
-  def config_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  end
+  include DeviseWhitelist
 end
