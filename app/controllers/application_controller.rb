ee666d0330434a49bd5045e29bb469b3108a86f2
liyicky <liyicky@gmail.com>
2019-02-06 13:55:37 +0900

    added new parameters to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  before_action :config_params, if: :devise_controller?
+
+  def config_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
