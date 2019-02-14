4cd39ab36b05357a4c5cf563413a24e129df7d5b
liyicky <liyicky@gmail.com>
2019-02-14 14:20:10 +0900

    added nav bar and default header data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   include DeviseWhitelist, SetSource, CurrentUserConcern
+
+  before_action :set_page_defaults
+
+  def set_page_defaults
+    @page_title = current_user.first_name + '\'s Dinosaur Turds'
+    @seo_keywords = 'Jason Cheladyn portfolio'
+  end
 end
